46e42e88c6c8d674f277c127d5c7bbbf4768df07
Gabriel <gabriel.nascimenton.19@gmail.com>
2019-10-26 10:45:57 -0300

    atualizacao do projeto

--- site-cursos/src/pages/cursos/form/index.js
+++ site-cursos/src/pages/cursos/form/index.js
@@ -2,54 +2,60 @@ import React from 'react'
 
 export default class Formulario extends React.Component {
 
-
-    
-
-   
-
     render() {
         return (
             <div className="border-right pl-3 pr-3">
-                <h3 className="border-bottom">Formulario</h3>
+                <h3 className="border-bottom">Formulário</h3>
                 <form>
                     <div className="form-group row">
-                        <label htmlFor="codigo" className="col-sm-3 col-form-label">
+                        <label htmlFor="codigo"
+                            className="col-sm-3 col-form-label">
                             Código:
                         </label>
                         <div className="col-sm-9">
-                            <input type="number" className="form-control" id="codigo" value={this.state.codigo} onChange={(e) => this.alteraCampo(e.target.value)} />
+                            <input type="number"
+                                className="form-control" id="codigo" value={this.props.codigo} 
+                                onChange={(e) => {this.props.alteraCampos(e.target)}}/>
                         </div>
                     </div>
                     <div className="form-group row">
-                        <label htmlFor="descrição" className="col-sm-3 col-form-label">
+                        <label htmlFor="descrição"
+                            className="col-sm-3 col-form-label">
                             Descrição:
                         </label>
                         <div className="col-sm-9">
-                            <input type="text" className="form-control" id="descricao" value={this.state.descricao} onChange={(e) => this.alteraCampo(e.target.value)} />
-                        </div>
-                    </div>
-                    
-                        <div className="form-group row">
-                        <label htmlFor="cargaHoraria" className="col-sm-3 col-form-label"> Carga Horária:</label>
-
-                        <div className="col-sm-9">
-                            <input type="number"
-                                className="form-control" id="cargaHoraria" value={this.state.cargaHoraria} onChange={(e) => this.alteraCampo(e.target.value)} />
+                            <input type="text"
+                                className="form-control" id="descricao" value={this.props.descricao} 
+                                onChange={(e) => {this.props.alteraCampos(e.target)}}/>
                         </div>
                     </div>
 
                     <div className="form-group row">
-                        <label htmlFor="preco" className="col-sm-3 col-form-label">Preço:</label>
+                        <label htmlFor="cargaHoraria"
+                            className="col-sm-3 col-form-label">
+                            Carga Horária:</label>
                         <div className="col-sm-9">
-                            <input type="number" className="form-control" id="preco" value={this.state.preco} onChange={(e) => this.alteraCampo(e.target.value)}/>
+                            <input type="number"
+                                className="form-control" id="cargaHoraria" value={this.props.cargaHoraria} 
+                                onChange={(e) => {this.props.alteraCampos(e.target)}}/>
                         </div>
                     </div>
-
+                    <div className="form-group row">
+                        <label htmlFor="preco"
+                            className="col-sm-3 col-form-label">
+                            Preço:</label>
+                        <div className="col-sm-9">
+                            <input type="number"
+                                className="form-control" id="preco"  value={this.props.preco}
+                                onChange={(e) => {this.props.alteraCampos(e.target)}}/>
+                        </div>
+                    </div>
                     <div className="form-group row">
                         <label htmlFor="categoria"
                             className="col-sm-3 col-form-label">Categoria:</label>
                         <div className="col-sm-9">
-                            <select className="form-control" id="categoria" value={this.state.categoria} onChange={(e) => this.alteraCampo(e.target.value)}>
+                            <select className="form-control" id="categoria" value={this.props.categoria}
+                                onChange={(e) => {this.props.alteraCampos(e.target)}}>
                                 <option>INFORMATICA</option>
                                 <option>ENGENHARIA</option>
                                 <option>ADMINISTRACAO</option>
@@ -58,7 +64,10 @@ export default class Formulario extends React.Component {
                         </div>
                     </div>
                     <div className="form-group row">
-                        <button className="btn btn-dark text-white  ml-3 mb-3"> Adicionar</button>
+                        <button onClick={this.props.adicionarCurso}
+                            className="btn btn-primary ml-3 mb-3">
+                            Adicionar
+                        </button>
                     </div>
                 </form>
             </div>
--- site-cursos/src/pages/cursos/lista/index.js
+++ site-cursos/src/pages/cursos/lista/index.js
@@ -4,33 +4,37 @@ export default class Lista extends React.Component {
 
     exibirLinhas = () => {
         const cursos = this.props.batatas || [];
-        return cursos.map(curso => ( 
-
+        return cursos.map(curso => (
             <tr key={curso._id}>
                 <td>{curso.codigo}</td>
                 <td>{curso.descricao}</td>
+                <td><button className="btn btn-danger"
+                    onClick={() => this.props.removerCurso(curso)}>
+                    <i className="fa fa-trash-o"></i>
+                </button>
+                </td>
             </tr>
-            
         ));
     }
 
     render() {
-            
-            return (
-                <div>
-                    <h3>Lista de Cursos</h3>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Código</th>
-                                <th>Descrição</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.exibirLinhas()}
-                        </tbody>
-                    </table>
-                </div>
-            )
+
+        return (
+            <div>
+                <h3>Lista de Cursos</h3>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Descrição</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.exibirLinhas()}
+                    </tbody>
+                </table>
+            </div>
+        )
     }
 }
